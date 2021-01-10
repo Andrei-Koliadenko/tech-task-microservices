232c375dcbe1080a32b0a77f3f2a35b8c5a9a55d
andyc <andyc@DESKTOP-MGNA2U2.mshome.net>
2021-01-11 00:21:47 +0200

    Add ApiConstants

---
+++ tech-task-some-controller/src/main/java/com/technical/task/api/ApiConstants.java
@@ -0,0 +1,8 @@
+package com.technical.task.api;
+
+public interface ApiConstants {
+	String SOME = "/api/resource";
+	String SOME_ID = "/api/resource/{id}";
+	String INSTANCE = "/api/resource/instance/";
+	String INSTANCE_ID = "/api/resource/instance/{id}";
+}
