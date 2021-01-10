d046f66eddffd3b45a6a434eb7356c51529eb869
andyc <andyc@DESKTOP-MGNA2U2.mshome.net>
2021-01-11 00:27:52 +0200

    update

---
+++ tech-task-instance-url/src/main/java/com/technical/task/component/InstanceUrl.java
@@ -0,0 +1,82 @@
+package com.technical.task.component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableDiscoveryClient
+public class InstanceUrl {
+	static Logger LOG = LoggerFactory.getLogger(InstanceUrl.class);
+	@Autowired
+	DiscoveryClient dc;
+	HashMap<String, HashSet<String>> serviceInstances = new HashMap<>(); // key - service name; value: set of all
+																			// instances
+	HashMap<String, Instant> serviceLastDiscovery = new HashMap<>();// key -service name, value instant time of last
+																	// discovery
+	@Value("${app-discovery-period-seconds:30}")
+	long discoveryPeriodSec;
+
+	public synchronized String getServiceUrl(String serviceName) {
+		String res = "";
+		HashSet<String> instances = serviceInstances.getOrDefault(serviceName, new HashSet<>());
+		if (serviceLastDiscovery.get(serviceName) == null || instances.isEmpty()
+				|| ChronoUnit.SECONDS.between(serviceLastDiscovery.getOrDefault(serviceName, Instant.now()),
+						Instant.now()) > discoveryPeriodSec) {
+			updateServiceInstances(serviceName, instances);
+		}
+		if (!instances.isEmpty()) {
+			res = getUrl(instances);
+		}
+		return res;
+	}
+	
+	public synchronized HashSet<String> getInstancesUrl(String serviceName) {
+//		String res = "";
+		HashSet<String> instances = serviceInstances.getOrDefault(serviceName, new HashSet<>());
+		if (serviceLastDiscovery.get(serviceName) == null || instances.isEmpty()
+				|| ChronoUnit.SECONDS.between(serviceLastDiscovery.getOrDefault(serviceName, Instant.now()),
+						Instant.now()) > discoveryPeriodSec) {
+			updateServiceInstances(serviceName, instances);
+		}
+		return instances;
+//		if (!instances.isEmpty()) {
+//			res = getUrl(instances);
+//		}
+//		return res;
+	}
+
+	
+
+	private void updateServiceInstances(String serviceName, HashSet<String> instances) {
+		HashSet<String> upToDateInstances = dc.getInstances(serviceName).stream().map(si -> si.getUri().toString())
+				.collect(Collectors.toCollection(HashSet::new));
+		LOG.debug("service: {}, count of instances: {}", serviceName, upToDateInstances.size());
+		instances.retainAll(upToDateInstances); // remained only relevant instances
+		instances.addAll(upToDateInstances); // added new instances
+		serviceLastDiscovery.put(serviceName, Instant.now());
+		serviceInstances.putIfAbsent(serviceName, instances);
+	}
+	
+
+	private String getUrl(HashSet<String> instances) {
+		// implementation of RRA (Round Robin Algorithm) algorithm
+		Iterator<String> it = instances.iterator();
+		String res = it.next();
+		//it.remove();
+		//instances.add(res);
+
+		return res;
+	}
+}
