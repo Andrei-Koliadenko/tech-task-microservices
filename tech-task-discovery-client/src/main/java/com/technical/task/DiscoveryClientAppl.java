6b5ea968dfbc865a99379cd986d029f74c3e008f
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-18 10:56:18 +0200

    update discovery client

--- tech-task-discovery-client/src/main/java/com/technical/task/DiscoveryClientAppl.java
+++ tech-task-discovery-client/src/main/java/com/technical/task/DiscoveryClientAppl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -23,8 +22,11 @@ public class DiscoveryClientAppl {
 
 		while (true) {
 			List<String> services = dc.getServices();
-			services.forEach(s -> {
-				System.out.printf("Service: %s, url: %s\n", s, iu.getServiceUrl(s));
+			services.forEach(serviceName -> {
+				List<String> instances = iu.getInstancesUrl(serviceName);
+				instances.forEach(instance -> {
+					System.out.printf("Service: %s, url: %s\n", serviceName, instance);
+				});
 			});
 
 			Thread.sleep(30000);
