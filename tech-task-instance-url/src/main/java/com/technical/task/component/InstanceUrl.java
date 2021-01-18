60edd10e9d1b4752873490b9c0e30b5b1df0cd76
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-18 10:56:38 +0200

    update instanceUrl

--- tech-task-instance-url/src/main/java/com/technical/task/component/InstanceUrl.java
+++ tech-task-instance-url/src/main/java/com/technical/task/component/InstanceUrl.java
@@ -5,7 +5,9 @@ import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -19,18 +21,19 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableDiscoveryClient
 public class InstanceUrl {
+
 	static Logger LOG = LoggerFactory.getLogger(InstanceUrl.class);
 	@Autowired
 	DiscoveryClient dc;
-	HashMap<String, HashSet<String>> serviceInstances = new HashMap<>(); // key - service name; value: set of all
-																			// instances
+	HashMap<String, LinkedHashSet<String>> serviceInstances = new HashMap<>(); // key - service name; value: set of all
+																				// instances
 	HashMap<String, Instant> serviceLastDiscovery = new HashMap<>();// key -service name, value instant time of last
 																	// discovery
 	@Value("${app-discovery-period-seconds:30}")
 	long discoveryPeriodSec;
-	
+
 	public synchronized List<String> getInstancesUrl(String serviceName) {
-		HashSet<String> instances = serviceInstances.getOrDefault(serviceName, new HashSet<>());
+		LinkedHashSet<String> instances = serviceInstances.getOrDefault(serviceName, new LinkedHashSet<>());
 		if (serviceLastDiscovery.get(serviceName) == null || instances.isEmpty()
 				|| ChronoUnit.SECONDS.between(serviceLastDiscovery.getOrDefault(serviceName, Instant.now()),
 						Instant.now()) > discoveryPeriodSec) {
@@ -42,36 +45,36 @@ public class InstanceUrl {
 
 	public synchronized String getServiceUrl(String serviceName) {
 		String res = "";
-		HashSet<String> instances = serviceInstances.getOrDefault(serviceName, new HashSet<>());
+		LinkedHashSet<String> instances = serviceInstances.getOrDefault(serviceName, new LinkedHashSet<>());
 		if (serviceLastDiscovery.get(serviceName) == null || instances.isEmpty()
 				|| ChronoUnit.SECONDS.between(serviceLastDiscovery.getOrDefault(serviceName, Instant.now()),
 						Instant.now()) > discoveryPeriodSec) {
 			updateServiceInstances(serviceName, instances);
 		}
 		if (!instances.isEmpty()) {
-			res = getUrl(instances);
+			res = getAndUpdateUrl(instances);
 		}
+
 		return res;
 	}
 
-	private void updateServiceInstances(String serviceName, HashSet<String> instances) {
-		HashSet<String> upToDateInstances = dc.getInstances(serviceName).stream().map(si -> si.getUri().toString())
-				.collect(Collectors.toCollection(HashSet::new));
-		LOG.debug("service: {}, count of instances: {}", serviceName, upToDateInstances.size());
-		instances.retainAll(upToDateInstances); // remained only relevant instances
-		instances.addAll(upToDateInstances); // added new instances
-		serviceLastDiscovery.put(serviceName, Instant.now());
-		serviceInstances.putIfAbsent(serviceName, instances);
-	}
-	
-
-	private String getUrl(HashSet<String> instances) {
+	private String getAndUpdateUrl(LinkedHashSet<String> instances) {
 		// implementation of RRA (Round Robin Algorithm) algorithm
 		Iterator<String> it = instances.iterator();
 		String res = it.next();
 		it.remove();
 		instances.add(res);
-
 		return res;
 	}
+
+	private void updateServiceInstances(String serviceName, LinkedHashSet<String> instances) {
+		LinkedHashSet<String> upToDateInstances = dc.getInstances(serviceName).stream()
+				.map(si -> si.getUri().toString()).collect(Collectors.toCollection(LinkedHashSet::new));
+		LOG.debug("service:{}, count of instances: {}", serviceName, upToDateInstances.size());
+		instances.retainAll(upToDateInstances); // remained only relevant instances
+		instances.addAll(upToDateInstances); // added new instances
+		serviceLastDiscovery.put(serviceName, Instant.now());
+		serviceInstances.putIfAbsent(serviceName, instances);
+	}
+
 }
